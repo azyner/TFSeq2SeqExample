f6afe9ec91302040e01799a72283f2b2fb02fc09
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2016-08-22 15:13:11 +1000

    Cleaned up model code, cut out buckets and cross entropy
    Commit before more code culling

--- model.py
+++ model.py
@@ -4,7 +4,12 @@ import numpy as np
 
 class Seq2SeqModel(object):
 
-    def __init__(self,parameters):
+    def __init__(self, parameters, generate):
+        max_gradient_norm = 5.0
+        size = 10
+        num_layers = 2
+        dtype = tf.float32
+        batch_size = 17
 
         #TODO
         #killall:
@@ -25,6 +30,8 @@ class Seq2SeqModel(object):
         # The seq2seq function: we use embedding for the input and attention.
         def seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
             return tf.nn.seq2seq.basic_rnn_seq2seq(encoder_inputs,decoder_inputs,cell,dtype=dtype)
+            #basic_rnn_seq2seq returns rnn_decoder returns output, state
+            #there is no loss function here
           # return tf.nn.seq2seq.embedding_attention_seq2seq(
           #     encoder_inputs,
           #     decoder_inputs,
@@ -36,163 +43,53 @@ class Seq2SeqModel(object):
           #     feed_previous=do_decode,
           #     dtype=dtype)
 
-        # Feeds for inputs.
-        self.encoder_inputs = []
-        self.decoder_inputs = []
-        self.target_weights = []
-        for i in xrange(buckets[-1][0]):  # Last bucket is the biggest one.
-          self.encoder_inputs.append(tf.placeholder(tf.int32, shape=[batch_size],
-                                                    name="encoder{0}".format(i)))
-        for i in xrange(buckets[-1][1] + 1):
-          self.decoder_inputs.append(tf.placeholder(tf.int32, shape=[batch_size],
-                                                    name="decoder{0}".format(i)))
-          self.target_weights.append(tf.placeholder(dtype, shape=[batch_size],
-                                                    name="weight{0}".format(i)))
+        # Feeds for input - no longer a list of buckets, just one will do.
+            #NOTE Weights:
+                #This is a list of weights used to determine the most important (favourable?) output. It weights the loss function
+                #Used in sequence_loss_by_example.
+        #NOTE - DECODERS:
+            # They are the correct prediction path. Useful to correct errors in training
+            # Need to do more research in the architecture here
 
+        self.encoder_input = tf.placeholder(tf.int32, shape=[batch_size], name="encoder")
+        self.decoder_input = tf.placeholder(tf.int32, shape=[batch_size], name="decoder")
+        self.target_weight = tf.placeholder(tf.int32, shape=[batch_size], name="target_weight")
+        self.target = tf.placeholder(tf.int32,shape=[batch_size],name="target")
         # Our targets are decoder inputs shifted by one.
+        #Alex - True for my model as well
         targets = [self.decoder_inputs[i + 1]
                    for i in xrange(len(self.decoder_inputs) - 1)]
 
+        # Alex - I don't know what the difference is between forward only and not. (refactored to generate)
         # Training outputs and losses.
-        if forward_only: #Test
-          self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(
-              self.encoder_inputs, self.decoder_inputs, targets,
-              self.target_weights, buckets, lambda x, y: seq2seq_f(x, y, True),
-              softmax_loss_function=softmax_loss_function)
-          # If we use output projection, we need to project outputs for decoding.
-          if output_projection is not None:
-            for b in xrange(len(buckets)):
-              self.outputs[b] = [
-                  tf.matmul(output, output_projection[0]) + output_projection[1]
-                  for output in self.outputs[b]
-              ]
+        if generate: #Test
+            self.output, self.internal_state = seq2seq_f(self.encoder_inputs, self.decoder_inputs, generate)
         else: #Training
-          self.outputs, self.losses = tf.nn.seq2seq.model_with_buckets(
-              self.encoder_inputs, self.decoder_inputs, targets,
-              self.target_weights, buckets,
-              lambda x, y: seq2seq_f(x, y, False),
-              softmax_loss_function=softmax_loss_function)
+            self.output, self.internal_state = seq2seq_f(self.encoder_inputs, self.decoder_inputs, generate)
+
+        def RMSE(x,y):
+            return  tf.sqrt(tf.reduce_mean(tf.square(tf.sub(y, x))))
+        weights = np.ones(len(self.target))#list of 1's the size of self.target
+
+        # TODO There are several types of cost functions to compare tracks. Implement many
+        # Mainly, average MSE over the whole track, or just at a horizon time (t+10 or something)
+        # There's this corner alg that Social LSTM refernces, but I haven't looked into it.
+
+        self.loss = tf.nn.seq2seq.sequence_loss_by_example(self.output,self.target,weights,softmax_loss_function=lambda x, y: RMSE(x,y))
 
         # Gradients and SGD update operation for training the model.
         params = tf.trainable_variables()
-        if not forward_only:
-          self.gradient_norms = []
-          self.updates = []
-          opt = tf.train.GradientDescentOptimizer(self.learning_rate)
-          for b in xrange(len(buckets)):
-            gradients = tf.gradients(self.losses[b], params)
-            clipped_gradients, norm = tf.clip_by_global_norm(gradients,
-                                                             max_gradient_norm)
+        if not generate:
+            self.gradient_norms = []
+            self.updates = []
+            opt = tf.train.GradientDescentOptimizer(self.learning_rate)
+            gradients = tf.gradients(self.loss, params)
+            clipped_gradients, norm = tf.clip_by_global_norm(gradients, max_gradient_norm)
+
             self.gradient_norms.append(norm)
             self.updates.append(opt.apply_gradients(
                 zip(clipped_gradients, params), global_step=self.global_step))
 
         self.saver = tf.train.Saver(tf.all_variables())
 
-        return -1
 
-    #TODO There are several types of cost functions to compare tracks. Implement many
-    #Mainly, average MSE over the whole track, or just at a horizon time (t+10 or something)
-    def sequence_loss_by_example_linear():
-        #The original sequence_loss_by_example assumes logits and is probably for word processing
-        #Or, I could just insert a MSE loss in the crossentropy loss function. Easier.
-
-
-        #To follow the same structure:
-
-        """Weighted loss for a sequence of logits, per-example
-            Args:
-                logits: List of 2D Tensors of shape [batch_size x num_decoder_symbols].
-                targets: List of 1D batch-sized int32 Tensors of the same length as logits.
-                weights: List of 1D batch-sized float-Tensors of the same length as logits.
-                    average_across_timesteps: If set, divide the returned cost by the total
-                    label weight.
-                    average_across_batch: If set, divide the returned cost by the batch size.
-                    softmax_loss_function: Function (inputs-batch, labels-batch) -> loss-batch
-                    to be used instead of the standard softmax (the default if this is None).
-                name: Optional name for this operation, defaults to "sequence_loss".
-            Returns:
-                A scalar float Tensor: The average log-perplexity per symbol (weighted).
-            Raises:
-                ValueError: If len(logits) is different from len(targets) or len(weights).
-        """
-        return -1
-    def loss():
-
-        return -1
-
-
-    # Returns the LSTM stack created based on the parameters.
-    # Processes several batches at once.
-    # Input shape is: (parameters['batch_size'], parameters['n_steps'], parameters['n_input'])
-    def RNN(parameters, input, model, initial_state):
-        # The model is:
-        # 1. input
-        # 2. linear layer
-        # 3 - n. LSTM layers
-        # n+1. linear layer
-        # n+1. output
-
-        # 1. layer, linear activation for each batch and step.
-        if (model.has_key('input_weights')):
-            input = tf.matmul(input, model['input_weights']) + model['input_bias']
-            # input = tf.nn.dropout(input, model['keep_prob'])
-
-        # # Split data because rnn cell needs a list of inputs for the RNN inner loop,
-        # # that is, a n_steps length list of tensors shaped: (batch_size, n_inputs)
-        #Alex -- Plenty of documentation in seq2seq
-        # # This is not well documented, but check for yourself here: https://goo.gl/NzA5pX
-        # input = tf.split(0, parameters['n_steps'], input)  # n_steps * (batch_size, :)
-
-        # Note: States is shaped: batch_size x cell.state_size
-        outputs, states = tf.nn.rnn(model['rnn_cell'], input, initial_state=initial_state)
-
-
-        # outputs[-1] = tf.Print(outputs[-1], [outputs[-1]], "LSTM Output: ", summarize = 100)
-        lastOutput = tf.verify_tensor_all_finite(outputs[-1], "LSTM Outputs not finite!")
-
-
-        # lastOutput = tf.nn.dropout(lastOutput, model['keep_prob'])
-        # Only the last output is interesting for error back propagation and prediction.
-        # Note that all batches are handled together here.
-        raw_output = tf.matmul(lastOutput, model['output_weights']) + model['output_bias']
-        raw_output = tf.verify_tensor_all_finite(raw_output, "Raw output not finite!")
-
-        # And now, instead of just outputting the expected value, we output mixture distributions.
-        # The number of mixtures is intuitively the number of possible actions the target can take.
-        # The output is divided into triplets of n_mixtures mixture parameters for the 2 absolute position coordinates.
-        if parameters['MDN']:
-            output = softmax_mixtures(raw_output, n_mixtures, batch_size)
-        else:
-            output = raw_output
-        # output = tf.Print(output, [output], "Output: ", summarize = 100)
-        output = tf.verify_tensor_all_finite(output, "Final output not finite!")
-
-        return (output, states)
-
-
-    def create(parameters):
-        print "Creating the neural network graph."
-        tf.reset_default_graph()
-
-        x = tf.placeholder(tf.float32, shape=(None, parameters['n_steps'], parameters['n_input']), name='input')
-        y = tf.placeholder(tf.float32, shape=(None, parameters['n_output']), name='expected_output')
-        #?????????????
-        lstm_state_size = np.sum(parameters['lstm_layers']) * 2
-        istate = tf.placeholder(tf.float32, shape=(None, lstm_state_size), name='internal_state')
-        lr = tf.placeholder(tf.float32, name='learning_rate')
-        input_size = 1 ##PLACEHOLDER
-
-        RNNInputSize = parameters['n_input']
-
-
-
-        return -1
-
-
-    def training():
-        return -1
-
-    def generate():
-        #Is this just a decoder wrapper?
-        return -1
--- train.py
+++ train.py
@@ -5,3 +5,80 @@
 #training loop, report losses, etc
 
 #declare session, checkpoints, loading etc
+
+
+
+
+
+
+# Returns the LSTM stack created based on the parameters.
+# Processes several batches at once.
+# Input shape is: (parameters['batch_size'], parameters['n_steps'], parameters['n_input'])
+def RNN(parameters, input, model, initial_state):
+    # The model is:
+    # 1. input
+    # 2. linear layer
+    # 3 - n. LSTM layers
+    # n+1. linear layer
+    # n+1. output
+
+    # 1. layer, linear activation for each batch and step.
+    if (model.has_key('input_weights')):
+        input = tf.matmul(input, model['input_weights']) + model['input_bias']
+        # input = tf.nn.dropout(input, model['keep_prob'])
+
+    # # Split data because rnn cell needs a list of inputs for the RNN inner loop,
+    # # that is, a n_steps length list of tensors shaped: (batch_size, n_inputs)
+    # Alex -- Plenty of documentation in seq2seq
+    # # This is not well documented, but check for yourself here: https://goo.gl/NzA5pX
+    # input = tf.split(0, parameters['n_steps'], input)  # n_steps * (batch_size, :)
+
+    # Note: States is shaped: batch_size x cell.state_size
+    outputs, states = tf.nn.rnn(model['rnn_cell'], input, initial_state=initial_state)
+
+    # outputs[-1] = tf.Print(outputs[-1], [outputs[-1]], "LSTM Output: ", summarize = 100)
+    lastOutput = tf.verify_tensor_all_finite(outputs[-1], "LSTM Outputs not finite!")
+
+    # lastOutput = tf.nn.dropout(lastOutput, model['keep_prob'])
+    # Only the last output is interesting for error back propagation and prediction.
+    # Note that all batches are handled together here.
+    raw_output = tf.matmul(lastOutput, model['output_weights']) + model['output_bias']
+    raw_output = tf.verify_tensor_all_finite(raw_output, "Raw output not finite!")
+
+    # And now, instead of just outputting the expected value, we output mixture distributions.
+    # The number of mixtures is intuitively the number of possible actions the target can take.
+    # The output is divided into triplets of n_mixtures mixture parameters for the 2 absolute position coordinates.
+    if parameters['MDN']:
+        output = softmax_mixtures(raw_output, n_mixtures, batch_size)
+    else:
+        output = raw_output
+    # output = tf.Print(output, [output], "Output: ", summarize = 100)
+    output = tf.verify_tensor_all_finite(output, "Final output not finite!")
+
+    return (output, states)
+
+
+def create(parameters):
+    print "Creating the neural network graph."
+    tf.reset_default_graph()
+
+    x = tf.placeholder(tf.float32, shape=(None, parameters['n_steps'], parameters['n_input']), name='input')
+    y = tf.placeholder(tf.float32, shape=(None, parameters['n_output']), name='expected_output')
+    # ?????????????
+    lstm_state_size = np.sum(parameters['lstm_layers']) * 2
+    istate = tf.placeholder(tf.float32, shape=(None, lstm_state_size), name='internal_state')
+    lr = tf.placeholder(tf.float32, name='learning_rate')
+    input_size = 1  ##PLACEHOLDER
+
+    RNNInputSize = parameters['n_input']
+
+    return -1
+
+
+def training():
+    return -1
+
+
+def generate():
+    # Is this just a decoder wrapper?
+    return -1
