2790c65011dc2f7ca47b327ced5a6676d9d2eac1
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2016-08-23 09:49:15 +1000

    Major edits to training, still not functional

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+#THIS IS A HEAVILY MODIFIED VERSION OF:
+#https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/rnn/translate/translate.py
+
 #load params
 
 #sort data into feed_dicts
@@ -5,80 +8,200 @@
 #training loop, report losses, etc
 
 #declare session, checkpoints, loading etc
+from seq2seq_model import Seq2SeqModel
+import tensorflow as tf
+
+import math
+import os
+import random
+import sys
+import time
+
+
+tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
+                          "Learning rate decays by this much.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
+                          "Clip gradients to this norm.")
+tf.app.flags.DEFINE_integer("batch_size", 64,
+                            "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("size", 1024, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("num_layers", 3, "Number of layers in the model.")
+tf.app.flags.DEFINE_integer("en_vocab_size", 40000, "English vocabulary size.")
+tf.app.flags.DEFINE_integer("fr_vocab_size", 40000, "French vocabulary size.")
+tf.app.flags.DEFINE_string("data_dir", "/tmp", "Data directory")
+tf.app.flags.DEFINE_string("train_dir", "/tmp", "Training directory.")
+tf.app.flags.DEFINE_integer("max_train_data_size", 0,
+                            "Limit on the size of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
+                            "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("decode", False,
+                            "Set to True for interactive decoding.")
+tf.app.flags.DEFINE_boolean("self_test", False,
+                            "Run a self-test if this is set to True.")
+tf.app.flags.DEFINE_boolean("use_fp16", False,
+                            "Train using fp16 instead of fp32.")
+
+FLAGS = tf.app.flags.FLAGS
 
 
 
-
-
-
-# Returns the LSTM stack created based on the parameters.
-# Processes several batches at once.
-# Input shape is: (parameters['batch_size'], parameters['n_steps'], parameters['n_input'])
-def RNN(parameters, input, model, initial_state):
-    # The model is:
-    # 1. input
-    # 2. linear layer
-    # 3 - n. LSTM layers
-    # n+1. linear layer
-    # n+1. output
-
-    # 1. layer, linear activation for each batch and step.
-    if (model.has_key('input_weights')):
-        input = tf.matmul(input, model['input_weights']) + model['input_bias']
-        # input = tf.nn.dropout(input, model['keep_prob'])
-
-    # # Split data because rnn cell needs a list of inputs for the RNN inner loop,
-    # # that is, a n_steps length list of tensors shaped: (batch_size, n_inputs)
-    # Alex -- Plenty of documentation in seq2seq
-    # # This is not well documented, but check for yourself here: https://goo.gl/NzA5pX
-    # input = tf.split(0, parameters['n_steps'], input)  # n_steps * (batch_size, :)
-
-    # Note: States is shaped: batch_size x cell.state_size
-    outputs, states = tf.nn.rnn(model['rnn_cell'], input, initial_state=initial_state)
-
-    # outputs[-1] = tf.Print(outputs[-1], [outputs[-1]], "LSTM Output: ", summarize = 100)
-    lastOutput = tf.verify_tensor_all_finite(outputs[-1], "LSTM Outputs not finite!")
-
-    # lastOutput = tf.nn.dropout(lastOutput, model['keep_prob'])
-    # Only the last output is interesting for error back propagation and prediction.
-    # Note that all batches are handled together here.
-    raw_output = tf.matmul(lastOutput, model['output_weights']) + model['output_bias']
-    raw_output = tf.verify_tensor_all_finite(raw_output, "Raw output not finite!")
-
-    # And now, instead of just outputting the expected value, we output mixture distributions.
-    # The number of mixtures is intuitively the number of possible actions the target can take.
-    # The output is divided into triplets of n_mixtures mixture parameters for the 2 absolute position coordinates.
-    if parameters['MDN']:
-        output = softmax_mixtures(raw_output, n_mixtures, batch_size)
+def create_model(session, train_model):
+    parameters = None
+    model = Seq2SeqModel(parameters,not train_model)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+    if ckpt and tf.gfile.Exists(ckpt.model_checkpoint_path):
+        print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
+        model.saver.restore(session, ckpt.model_checkpoint_path)
     else:
-        output = raw_output
-    # output = tf.Print(output, [output], "Output: ", summarize = 100)
-    output = tf.verify_tensor_all_finite(output, "Final output not finite!")
+        print("Created model with fresh parameters.")
+        session.run(tf.initialize_all_variables())
+    return model
 
-    return (output, states)
-
-
-def create(parameters):
-    print "Creating the neural network graph."
-    tf.reset_default_graph()
-
-    x = tf.placeholder(tf.float32, shape=(None, parameters['n_steps'], parameters['n_input']), name='input')
-    y = tf.placeholder(tf.float32, shape=(None, parameters['n_output']), name='expected_output')
-    # ?????????????
-    lstm_state_size = np.sum(parameters['lstm_layers']) * 2
-    istate = tf.placeholder(tf.float32, shape=(None, lstm_state_size), name='internal_state')
-    lr = tf.placeholder(tf.float32, name='learning_rate')
-    input_size = 1  ##PLACEHOLDER
-
-    RNNInputSize = parameters['n_input']
-
-    return -1
+#TODO
+#TUESDAY:
+#Follow the training steps. Why is the optimzer not here?
 
 
-def training():
-    return -1
+
+def train():
+  """Train a en->fr translation model using WMT data."""
+  # Prepare WMT data.
+  #PREPARE DATA INTO:
+  #[train test validate] x [input_tracks, output_tracks]
+
+  with tf.Session() as sess:
+    # Create model.
+    print("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
+    model = create_model(sess, False)
+
+    # Read data into buckets and compute their sizes.
+    print ("Reading development and training data (limit: %d)."
+           % FLAGS.max_train_data_size)
+    dev_set = test_data?
+    train_set = get_data
+
+    #Replace with minibatch sizes?
+
+    #train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+    train_total_size = float(sum(train_bucket_sizes))
+
+    # This is the training loop.
+    step_time, loss = 0.0, 0.0
+    current_step = 0
+    previous_losses = []
+    while True:
 
 
-def generate():
-    # Is this just a decoder wrapper?
-    return -1
+      # Get a batch and make a step.
+      start_time = time.time()
+
+      encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+          train_set, """bucket_id""")
+
+      _, step_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                   target_weights, """bucket_id""", False)
+      step_time += (time.time() - start_time) / FLAGS.steps_per_checkpoint
+      loss += step_loss / FLAGS.steps_per_checkpoint
+      current_step += 1
+
+      # Once in a while, we save checkpoint, print statistics, and run evals.
+      if current_step % FLAGS.steps_per_checkpoint == 0:
+        # Print statistics for the previous epoch.
+        perplexity = math.exp(float(loss)) if loss < 300 else float("inf")
+        print ("global step %d learning rate %.4f step-time %.2f perplexity "
+               "%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
+                         step_time, perplexity))
+        # Decrease learning rate if no improvement was seen over last 3 times.
+        if len(previous_losses) > 2 and loss > max(previous_losses[-3:]):
+          sess.run(model.learning_rate_decay_op)
+        previous_losses.append(loss)
+        # Save checkpoint and zero timer and loss.
+        checkpoint_path = os.path.join(FLAGS.train_dir, "translate.ckpt")
+        model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+        step_time, loss = 0.0, 0.0
+        # Run evals on development set and print their perplexity.
+
+        encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+          dev_set, bucket_id)
+        _, eval_loss, _ = model.step(sess, encoder_inputs, decoder_inputs,
+                                   target_weights, bucket_id, True)
+        eval_ppx = math.exp(float(eval_loss)) if eval_loss < 300 else float(
+          "inf")
+        print("  eval: run %d perplexity %.2f" % (iteration???, eval_ppx))
+        sys.stdout.flush()
+
+
+def decode():
+  with tf.Session() as sess:
+    # Create model and load parameters.
+    model = create_model(sess, True)
+    model.batch_size = 1  # We decode one sentence at a time.
+
+    # Load vocabularies.
+    en_vocab_path = os.path.join(FLAGS.data_dir,
+                                 "vocab%d.en" % FLAGS.en_vocab_size)
+    fr_vocab_path = os.path.join(FLAGS.data_dir,
+                                 "vocab%d.fr" % FLAGS.fr_vocab_size)
+    en_vocab, _ = data_utils.initialize_vocabulary(en_vocab_path)
+    _, rev_fr_vocab = data_utils.initialize_vocabulary(fr_vocab_path)
+
+    # Decode from standard input.
+    sys.stdout.write("> ")
+    sys.stdout.flush()
+    sentence = sys.stdin.readline()
+    while sentence:
+      # Get token-ids for the input sentence.
+      token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab)
+      # Which bucket does it belong to?
+      bucket_id = min([b for b in xrange(len(_buckets))
+                       if _buckets[b][0] > len(token_ids)])
+      # Get a 1-element batch to feed the sentence to the model.
+      encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+          {bucket_id: [(token_ids, [])]}, bucket_id)
+      # Get output logits for the sentence.
+      _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
+                                       target_weights, """bucket_id""", True)
+      # This is a greedy decoder - outputs are just argmaxes of output_logits.
+      outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+      # If there is an EOS symbol in outputs, cut them at that point.
+      if data_utils.EOS_ID in outputs:
+        outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+      # Print out French sentence corresponding to outputs.
+      print(" ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs]))
+      print("> ", end="")
+      sys.stdout.flush()
+      sentence = sys.stdin.readline()
+
+
+def self_test():
+  """Test the translation model."""
+  with tf.Session() as sess:
+    print("Self-test for neural translation model.")
+    # Create model with vocabularies of 10, 2 small buckets, 2 layers of 32.
+    model = Seq2SeqModel(10, 10, [(3, 3), (6, 6)], 32, 2,
+                                       5.0, 32, 0.3, 0.99, num_samples=8)
+    sess.run(tf.initialize_all_variables())
+
+    # Fake data set for both the (3, 3) and (6, 6) bucket.
+    data_set = ([([1, 1], [2, 2]), ([3, 3], [4]), ([5], [6])],
+                [([1, 1, 1, 1, 1], [2, 2, 2, 2, 2]), ([3, 3, 3], [5, 6])])
+    for _ in xrange(5):  # Train the fake model for 5 steps.
+      bucket_id = random.choice([0, 1])
+      encoder_inputs, decoder_inputs, target_weights = model.get_batch(
+          data_set, bucket_id)
+      model.step(sess, encoder_inputs, decoder_inputs, target_weights,
+                 bucket_id, False)
+
+
+
+def main(_):
+  if FLAGS.self_test:
+    self_test()
+  elif FLAGS.decode:
+    decode()
+  else:
+    train()
+
+if __name__ == "__main__":
+  tf.app.run()
