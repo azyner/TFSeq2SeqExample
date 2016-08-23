b11a4e12db432430890e01918861ffeeb9a60172
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2016-08-23 11:48:02 +1000

    Changed data_utils to match requirements (encoder/decoder seqences)

--- data_utils.py
+++ data_utils.py
@@ -31,7 +31,7 @@ def sin_cos(x):
     return pd.DataFrame(dict(a=np.sin(x), b=np.cos(x)), index=x)
 
 
-def rnn_data(data, time_steps, labels=False):
+def rnn_data(data, encoder_steps, decoder_steps):
     """
     creates new data frame based on previous observation
       * example:
@@ -40,17 +40,17 @@ def rnn_data(data, time_steps, labels=False):
         -> labels == False [[1, 2], [2, 3], [3, 4]]
         -> labels == True [2, 3, 4, 5]
     """
-    rnn_df = []
-    for i in range(len(data) - time_steps):
-        if labels:
-            try:
-                rnn_df.append([data.iloc[i + time_steps].as_matrix()])
-            except AttributeError:
-                rnn_df.append(data.iloc[i + time_steps])
-        else:
-            data_ = data.iloc[i: i + time_steps].as_matrix()
-            rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
-    return np.array(rnn_df)
+    rnn_df_encoder = []
+    rnn_df_decoder = []
+    #TODO ALEX Change range, return pair of data, encoder_data, decoder_data
+    for i in range(len(data) - (encoder_steps+decoder_steps)):
+        try:
+            rnn_df_decoder.append(data.iloc[i + encoder_steps:i +(encoder_steps+decoder_steps)].as_matrix())
+        except AttributeError:
+            rnn_df_decoder.append(data.iloc[i + encoder_steps:i +(encoder_steps+decoder_steps)])
+        data_ = data.iloc[i: i + encoder_steps].as_matrix()
+        rnn_df_encoder.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
+    return np.array(rnn_df_encoder), np.array(rnn_df_decoder)
 
 
 def split_data(data, val_size=0.1, test_size=0.1):
@@ -65,18 +65,26 @@ def split_data(data, val_size=0.1, test_size=0.1):
     return df_train, df_val, df_test
 
 
-def prepare_data(data, time_steps, labels=False, val_size=0.1, test_size=0.1):
+def prepare_data(data, encoder_steps, decoder_steps, labels=False, val_size=0.1, test_size=0.1):
     """
     Given the number of `time_steps` and some data,
     prepares training, validation and test data for an lstm cell.
     """
     df_train, df_val, df_test = split_data(data, val_size, test_size)
-    return (rnn_data(df_train, time_steps, labels=labels),
-            rnn_data(df_val, time_steps, labels=labels),
-            rnn_data(df_test, time_steps, labels=labels))
+    return (rnn_data(df_train, encoder_steps, decoder_steps),
+            rnn_data(df_val, encoder_steps, decoder_steps),
+            rnn_data(df_test, encoder_steps, decoder_steps))
 
-def generate_data(fct, x, fct_mod, time_steps, seperate=False):
-    """generates data with based on a function fct"""
+def generate_data(fct, x, fct_mod, encoder_steps, decoder_steps, seperate=False):
+
+    """generates data with based on a function fct
+    input:
+    fct: The function to be used to generate data (eg sin)
+    x: the linspace to pass to the function
+    fct mod: A list of elements of 4 tuples that represent function modifiers: a+b*fct(c+d*x)
+
+    """
+
     train_x, val_x, test_x = [],[],[]
     train_y, val_y, test_y = [],[],[]
 
@@ -91,30 +99,30 @@ def generate_data(fct, x, fct_mod, time_steps, seperate=False):
         if(len(fct_mod)==1):
             if not isinstance(data, pd.DataFrame):
                 data = pd.DataFrame(data)
-            w_train_x, w_val_x, w_test_x = prepare_data(data['a'] if seperate else data, time_steps)
-            w_train_y, w_val_y, w_test_y = prepare_data(data['b'] if seperate else data, time_steps, labels=True)
-            train_x.extend(w_train_x)
-            val_x.extend(w_val_x)
-            test_x.extend(w_test_x)
-            train_y.extend(w_train_y)
-            val_y.extend(w_val_y)
-            test_y.extend(w_test_y)
+            w_train, w_val, w_test = prepare_data(data['a'] if seperate else data, encoder_steps, decoder_steps)
+            train_x.extend(w_train[0])
+            val_x.extend(w_val[0])
+            test_x.extend(w_test[0])
+            train_y.extend(w_train[1])
+            val_y.extend(w_val[1])
+            test_y.extend(w_test[1])
         else:
             #training / val are most of data. Test is the last function.
             if(wave is not fct_mod[-1]):
                 if not isinstance(data, pd.DataFrame):
                     data = pd.DataFrame(data)
-                w_train_x, w_val_x, w_test_x = prepare_data(data['a'] if seperate else data, time_steps,test_size = 0)
-                w_train_y, w_val_y, w_test_y = prepare_data(data['b'] if seperate else data, time_steps,test_size = 0, labels=True)
-                train_x.extend(w_train_x)
-                val_x.extend(w_val_x)
-                train_y.extend(w_train_y)
-                val_y.extend(w_val_y)
+                w_train, w_val, w_test = prepare_data(data['a'] if seperate else data, encoder_steps, decoder_steps, test_size = 0)
+                train_x.extend(w_train[0])
+                val_x.extend(w_val[0])
+                test_x.extend(w_test[0])
+                train_y.extend(w_train[1])
+                val_y.extend(w_val[1])
+                test_y.extend(w_test[1])
             else:
+                #last function track, use for training
                 if not isinstance(data, pd.DataFrame):
                     data = pd.DataFrame(data)
-                test_x.extend(rnn_data(data, time_steps, labels=False))
-                test_y.extend(rnn_data(data, time_steps, labels=True))
+                test_x, test_y = rnn_data(data, encoder_steps, decoder_steps)
 
     return dict(train=np.array(train_x),
                 val=np.array(val_x),
@@ -168,69 +176,25 @@ PRINT_STEPS = TRAINING_STEPS / 100
 
 #Everything in learn is actually skflow. This is why this is so confusing, and why the session is abstracted.
 
-regressor = learn.TensorFlowEstimator(model_fn=lstm_model(TIMESTEPS, RNN_LAYERS, DENSE_LAYERS), n_classes=0,
-                                      verbose=2,  steps=TRAINING_STEPS, optimizer='Adagrad',
-                                      learning_rate=0.03, batch_size=BATCH_SIZE)
 
-X, y = generate_data(np.sin, np.linspace(0, 100, 10000), [#(0,  1, 0, 1),
-                                                          #(0,  2, 0, 5),
-                                                          #(0,  1, 1, 0.5),
-                                                          #(0,  1, 0, 5),
-                                                          #(1,  1, 0, 10),
-                                                          #(1,  3, 0, 5),
-                                                          #(1,  1, 1, 0.1),
-                                                          #(1,  2, 0, 5),
-                                                          #(1,  1, 0, 50),
-                                                          #(-1, 2, 0, 5),
-                                                          #(-1, 1, 1, 20),
-                                                          #(-1, 1, 0, 5),
-                                                          #(-1, 0.1, 0, 10),
-                                                          #(-1, 0.5, 0, 12),
-                                                          #(0,  -1, 0, 1),
-                                                          #(0,  -2, 0, 5),
-                                                          #(0,  -1, 1, 0.5),
-                                                          #(0,  -1, 0, 5),
-                                                          #(1,  -1, 0, 10),
-                                                          #(1,  -3, 0, 5),
-                                                          #(1,  -1, 1, 0.1),
-                                                          #(1,  -2, 0, 5),
-                                                          #(1,  -1, 0, 50),
-                                                          #(-1, -2, 0, 5),
-                                                          #(-1, -1, 1, 20),
-                                                          #(-1, -1, 0, 5),
-                                                          #(-1, -0.1, 0, 10),
-                                                          #(-1, 1.5, 0, 16),
+X, y = generate_data(np.sin, np.linspace(0, 100, 10000), [(0, 1, 0, 16),
                                                         (0, 1, 0, 16),
                                                         (0, 1, 0, 16),
                                                         (0, 1, 0, 16),
-                                                        (0, 1, 0, 16),
-                                                          ],TIMESTEPS, seperate=False)
+                                                          ],TIMESTEPS, TIMESTEPS, seperate=False)
 #New y format breaks this
 test_sequence = np.concatenate([X['test'][0],y['test']])
 
-# create a lstm instance and validation monitor #Val monitor has data in an incorrect format?
-validation_monitor = learn.monitors.ValidationMonitor(X['val'], y['val'],
-                                                      every_n_steps=PRINT_STEPS,
-                                                      early_stopping_rounds=1000)#early termination disable
-regressor.fit(X['train'], y['train'], monitors=[validation_monitor])#, logdir=LOG_DIR)
-
-
-predicted = regressor.predict(X['test'])
-predicted = generate_sequence(regressor,test_sequence,100,50)
 
 #The below is false. It still has a strange disjoint when it starts predicting though
 #BUG there is a chance the sequence generator is predicting backwards, which would explain the step at the beginning.
 #I find this strange, but there is an easy way to find out, stop feeding at a peak
 #This section here needs to be modified with a sequence generation function
 
-rmse = np.sqrt(((predicted - test_sequence[0:len(predicted)]) ** 2).mean(axis=0))
-score = mean_squared_error(predicted, test_sequence[0:len(predicted)])
-print ("MSE: %f" % score)
-
-plot_predicted, = plt.plot(predicted, label='predicted')
-plot_test, = plt.plot(test_sequence[0:len(predicted)], label='test')
-plt.legend(handles=[plot_predicted, plot_test])
-plt.show()
+# plot_predicted, = plt.plot(predicted, label='predicted')
+# plot_test, = plt.plot(test_sequence[0:len(predicted)], label='test')
+# plt.legend(handles=[plot_predicted, plot_test])
+# plt.show()
 
 quit()
 
