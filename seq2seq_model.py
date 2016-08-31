0ddd4aaf790b8404a2bee031c52d3240e2846cd3
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2016-08-31 19:04:42 +1000

    Further fixed state_is_tuple bug

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -44,7 +44,7 @@ class Seq2SeqModel(object):
         single_cell = tf.nn.rnn_cell.BasicLSTMCell(self.rnn_size,state_is_tuple=True) #Default should be True, but TF 0.9 was throwing a warning, implying it was false
         cell = single_cell
         if self.num_layers > 1:
-          cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * self.num_layers)
+          cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * self.num_layers,state_is_tuple=True) #Gah, the bug is here -- this defaults to False, and thus produces a warning....
 
         def simple_loop_function(prev, _):
             if output_projection is not None:
