3e80a7191d2951acb9a10270e741dfa4122fd441
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2016-09-02 13:57:36 +1000

    Added new function for RNN with last output feedforward

--- TF_mods.py
+++ TF_mods.py
@@ -12,3 +12,15 @@ def basic_rnn_seq2seq_with_loop_function(
     with variable_scope.variable_scope(scope or "basic_rnn_seq2seq_with_loop_function"):
         _, enc_state = rnn.rnn(cell, encoder_inputs, dtype=dtype)
         return rnn_decoder(decoder_inputs, enc_state, cell, loop_function=loop_function)
+
+# TODO Needs a better name
+def basic_rnn_seq2seq_with_loop_function_no_go_token(
+        encoder_inputs, decoder_inputs, cell, dtype=dtypes.float32, loop_function=None, scope=None):
+    """Basic RNN sequence-to-sequence model. Edited for a loopback function. Don't know why this isn't in the
+    current library
+    """
+
+    with variable_scope.variable_scope(scope or "basic_rnn_seq2seq_with_loop_function"):
+        outputs, enc_state = rnn.rnn(cell, encoder_inputs, dtype=dtype)
+        # decoder_inputs[0] = ouputs[-1] # Loop the last ignored prediction as the first input to the decoder
+        return rnn_decoder(decoder_inputs, enc_state, cell, loop_function=loop_function)
